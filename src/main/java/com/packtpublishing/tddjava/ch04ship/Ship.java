206ef13c9903e033603d708a6880bf71795c0407
johnson <zhangsen10@cnhqd.net>
2020-02-27 14:45:54 +0800

    forward and backward

--- src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
+++ src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
@@ -1,5 +1,25 @@
 package com.packtpublishing.tddjava.ch04ship;
 
 public class Ship {
+    private Location location;
 
+    public Ship(Location location) {
+        this.location = location;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public void moveForward() {
+        location.forward();
+    }
+
+    public void moveBackward() {
+        location.backward();
+    }
 }
--- src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
@@ -6,5 +6,35 @@ import static org.testng.Assert.*;
 
 @Test
 public class ShipSpec {
+    private Ship ship;
+    private Location location;
 
+    @BeforeMethod
+    public void setUp() {
+        location = new Location(new Point(21,13), Direction.NORTH);
+        ship = new Ship(location);
+    }
+
+    public void whenInstantiatedThenLocationIsSet() {
+        assertEquals(ship.getLocation(), location);
+    }
+
+
+    @Test
+    public void whenMoveForwardThenForward() {
+        Location expected = location.copy();
+        expected.forward();
+        ship.moveForward();
+
+        assertEquals(ship.getLocation(), expected);
+    }
+
+    @Test
+    public void whenMoveBackwardThenBackward() {
+        Location expected = location.copy();
+        expected.backward();
+        ship.moveBackward();
+
+        assertEquals(ship.getLocation(), expected);
+    }
 }
